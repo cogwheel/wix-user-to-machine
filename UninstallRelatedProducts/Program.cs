af6ee6ed7bf6c2138a25ec937f761e0c8c5157ac
Matthew Orlando <cogwheel@caffeine.tv>
2018-03-02 14:54:19 -0800

    Removed dependency on CommandLine library Only command line option is now the Upgrade Code

--- UninstallRelatedProducts/Program.cs
+++ UninstallRelatedProducts/Program.cs
@@ -1,58 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Text;
 
 namespace UninstallRelatedProducts
 {
     class Program
     {
+        const int errExit = 1;
+
         static void Main(string[] args)
         {
-            var options = Options.Parse(args);
-
-            using (var logger = new Logger(options.LogFilePath))
+            using (var logger = new Logger(null))
             {
+                Guid upgradeCode;
+                if (args.Length != 1 || !Guid.TryParse(args[0], out upgradeCode))
+                {
+                    logger.Log("usage:\n     UninstallRelatedProducts <GUID>");
+                    Environment.Exit(errExit);
+                    return; // To avoid uninitialized error
+                }
+
                 try
                 {
-                    // Additional arguments parsing
-                    var upgradeCode = Guid.Parse(options.UpgradeCode);
-                    var maxVersion = default(Version);
-                    if (!string.IsNullOrEmpty(options.MaxVersion))
-                        maxVersion = Version.Parse(options.MaxVersion);
-
                     var productCodes = Msi.GetRelatedProducts(upgradeCode).ToList();
                     logger.Log("Number of related products found: " + productCodes.Count);
 
                     foreach (var product in productCodes)
                     {
                         logger.Log("Product code: " + product);
-
-                        if (maxVersion != null)
+                        var allUsers = Msi.IsAllUsers(product);
+                        logger.Log("All users: " + allUsers);
+                        if (allUsers)
                         {
-                            var version = Msi.GetVersion(product);
-                            logger.Log("Product version: " + version);
-                            if (version > maxVersion)
-                            {
-                                logger.Log("Skipping.");
-                                continue;
-                            }
+                            Console.WriteLine("Skipping.");
+                            continue;
                         }
 
-                        if (options.PerUserOnly)
-                        {
-                            var allUsers = Msi.IsAllUsers(product);
-                            logger.Log("All users: " + allUsers);
-                            if (allUsers)
-                            {
-                                Console.WriteLine("Skipping.");
-                                continue;
-                            }
-                        }
-
-                        logger.Log("Uninstalling. Silently: " + options.Silent);
-                        Msi.Uninstall(product, options.Silent);
+                        Msi.Uninstall(product, silent: true);
                     }
 
                     logger.Log("Uninstall completed successfully");
